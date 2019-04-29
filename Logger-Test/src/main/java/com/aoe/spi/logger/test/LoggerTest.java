61f623f16c68a1bac097a999a4357d719cd98876
483879 <zhangyulong@sf-express.com>
2019-04-29 18:39:26 +0800

    add logger test projiect

---
+++ Logger-Test/src/main/java/com/aoe/spi/logger/test/LoggerTest.java
@@ -0,0 +1,23 @@
+package com.aoe.spi.logger.test;
+
+import org.framework.aoe.spi.logger.Logger;
+import org.framework.aoe.spi.logger.LoggerFactory;
+
+public class LoggerTest {
+
+	private final static Logger logger = LoggerFactory.getLogger();
+	
+	public static void main(String[] args) {
+		logger.debug("this is use debug... ");
+		logger.info("this is use info... ");
+		logger.warn("this is use warn... ");
+		logger.error("this is use error... ");
+	}
+
+}
+
+/**
+ * 
+ * 
+ * 
+ * **/
