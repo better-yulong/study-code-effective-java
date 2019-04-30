0b07733f20c696e1e623f2eac8d250e8e2e3b22d
483879 <zhangyulong@sf-express.com>
2019-04-30 17:33:54 +0800

    add logger test projiect

--- framework-aoe-parent/framework-aoe-spi-logger/src/main/java/org/framework/aoe/spi/logger/LoggerFactory.java
+++ framework-aoe-parent/framework-aoe-spi-logger/src/main/java/org/framework/aoe/spi/logger/LoggerFactory.java
@@ -1,28 +1,36 @@
 package org.framework.aoe.spi.logger;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.ServiceLoader;
 
 public class LoggerFactory {
 	
+	private static ArrayList<Logger> list ;
+    static {
+        list = new ArrayList<Logger>();
+
+        ServiceLoader loader = ServiceLoader.load(Logger.class);
+        Iterator<Logger> it = loader.iterator();
+        while (it.hasNext()) {
+            list.add(it.next());
+        }
+    }
+    
 	public LoggerFactory(){
 	}
 	
 	public static Logger getLogger(){
-		Logger logger = null ;
-		
-		ServiceLoader<Logger> serviceLoader = ServiceLoader.load(Logger.class);
-		Iterator<Logger> loggers = serviceLoader.iterator();
-		if(loggers.hasNext()){
-			logger = loggers.next();
-		}
-		return logger;
+		System.out.println(new Date());
+		return list!=null?list.get(0):null;
 	}
 	
 /*	
  * 如若多个实现只需返回一个，可参考如上添加根据入参返回不同实现。
  * 而如若需同时支持从个实现，而考虑基于参考如下实现类   类责任链模式 ，
+ * 参考：https://my.oschina.net/redraiment/blog/105209
+ * 
 	private static ArrayList list ;
     static {
         list = new ArrayList();
