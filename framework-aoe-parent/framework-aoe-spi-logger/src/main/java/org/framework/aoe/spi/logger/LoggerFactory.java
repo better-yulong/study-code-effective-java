a2e3c0069d5b2ce92a6da7068dc772b3ffc9a10a
483879 <zhangyulong@sf-express.com>
2019-04-30 17:19:31 +0800

    add logger test projiect

--- framework-aoe-parent/framework-aoe-spi-logger/src/main/java/org/framework/aoe/spi/logger/LoggerFactory.java
+++ framework-aoe-parent/framework-aoe-spi-logger/src/main/java/org/framework/aoe/spi/logger/LoggerFactory.java
@@ -1,5 +1,6 @@
 package org.framework.aoe.spi.logger;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.ServiceLoader;
 
@@ -18,5 +19,30 @@ public class LoggerFactory {
 		}
 		return logger;
 	}
+	
+/*	
+ * 如若多个实现只需返回一个，可参考如上添加根据入参返回不同实现。
+ * 而如若需同时支持从个实现，而考虑基于参考如下实现类   类责任链模式 ，
+	private static ArrayList list ;
+    static {
+        list = new ArrayList();
+
+        ServiceLoader loader = ServiceLoader.load(Logger.class);
+        Iterator it = loader.iterator();
+        while (it.hasNext()) {
+            list.add(it.next());
+        }
+    }
+    
+    public static void process(Properties options) {
+        for (Handler handler : list) {
+            if (handler.accept(options)) {
+                handler.execute();
+            }
+        }
+    }
+    
+    
+    */
 
 }
