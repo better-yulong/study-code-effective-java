7b044d11224d69245e533c3f17035808ee5e9493
better-yulong <better.yulong@gmail.com>
2019-07-17 17:38:15 +0800

    featuer:add dubbo source code

---
+++ rpc-server/src/main/java/com/aoe/demo/rpc/dubbo/DubboExampleService1.java
@@ -0,0 +1,15 @@
+package com.aoe.demo.rpc.dubbo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DubboExampleService1 implements DubboExampleInterf1 {
+
+	public List serviceProvider(List params) {
+		System.out.println("param0:" + params.get(0));
+		List serviceNames = new ArrayList<String>();
+		serviceNames.add("DubboExampleService1");
+		return serviceNames ;
+	}
+
+}
---
+++ rpc-skeleton/src/main/java/com/aoe/demo/rpc/dubbo/DubboExampleInterf1.java
@@ -0,0 +1,9 @@
+package com.aoe.demo.rpc.dubbo;
+
+import java.util.List;
+
+public interface DubboExampleInterf1 {
+	
+	public List serviceProvider(List params);
+
+}
